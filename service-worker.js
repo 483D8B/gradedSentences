d60ff0dfa0b5a44794b21095b621e7c2f59f2eae
christian <134725438+483D8B@users.noreply.github.com>
2024-06-01 12:00:09 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,4 @@
 // This is the "Offline copy of pages" service worker
-
 const CACHE = "pwabuilder-offline";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
@@ -10,9 +9,16 @@ self.addEventListener("message", (event) => {
   }
 });
 
+// Exclude the specific script from being cached
 workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.NetworkOnly({
+  new RegExp('https://483d8b.pythonanywhere.com/output.js'),
+  new workbox.strategies.NetworkOnly()
+);
+
+// Cache other files
+workbox.routing.registerRoute(
+  ({url}) => url.origin === location.origin && !url.pathname.endsWith('output.js'),
+  new workbox.strategies.StaleWhileRevalidate({
     cacheName: CACHE
   })
 );
